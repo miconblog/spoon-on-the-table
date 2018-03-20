adff15dde9b31f44338e807e2d740e8c784f7c67
불꽃남자 <miconblog@gmail.com>
2018-03-20 15:16:49 +0800

    withRedux HoC 컴포넌트에 인증 객체 자동으로 삽입하기

--- redux/withRedux.js
+++ redux/withRedux.js
@@ -23,8 +23,13 @@ export default (...args) => (Component) => {
   // First argument is initStore, the rest are redux connect arguments and get passed
   const [initStore, ...connectArgs] = args;
 
+  console.log('111111');
+
+
+  // 마지막에 동작하는 컴포넌트,..
   const ComponentWithRedux = (props = {}) => {
     const { store, initialProps, initialState, loginUser } = props;
+    console.log('444444');
 
     if (loginUser) {
       initialState.loginUser = loginUser;
@@ -43,18 +48,28 @@ export default (...args) => (Component) => {
     );
   };
 
+  // Next.js --> getInitialProps (S)
   ComponentWithRedux.getInitialProps = async (props = {}) => {
     const isServer = checkServer();
     const store = getOrCreateStore(initStore);
+    console.log('22222', isServer);
+
 
     // 스토어 객체에 인증정보를 같이 넣는다.
     const { req } = props;
     let loginUser = null;
+
     if (isServer && req.user) {
+      // for server
       loginUser = req.user.toJSON();
+      loginUser.profileImage = "https://scontent-hkg3-2.xx.fbcdn.net/v/t1.0-1/p320x320/1510494_10151938283373302_404117394_n.jpg?oh=fef97d5f281b11e85993992d276ee11f&oe=5B45B569";
+    } else {
+      // for client..
+      loginUser = store.getState().loginUser;
     }
 
-    // Run page getInitialProps with store and isServer
+    // Run page getInitialProps with store and isServer + loginUser
+    // 서버에서 동작: HOC 원본 컴포넌트에 스토어와 인증객체 같이 내려줌. 
     const initialProps = Component.getInitialProps
       ? await Component.getInitialProps({
         ...props,
@@ -64,7 +79,7 @@ export default (...args) => (Component) => {
       })
       : {};
 
-    // For Client 
+    // HOC 컴포넌트는 원본이 먼저 동작한 후에 반환된다. for Client
     return {
       store,
       loginUser,
