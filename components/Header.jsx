cb53d1c6b974a04ca8831f9be7b4db4ef83e2185
불꽃남자 <miconblog@gmail.com>
2018-03-20 15:14:54 +0800

    Next.js prefetch 잘 못 적용한 부분 개선
    <Link prefetch> 형식은 URL이 서버 페이지와 이름이 동일할때만 적용된다. 따라서 my/profile 처럼 라우터와 서버 페이지 파일명이 일치 하지 않은 경우 Router.perfetch 메소드를 이용해 직접 호출해야한다.
    이때 <Link>로 <a> 태그를 감싸면 제대로 동작하지 않는다.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,9 @@
-import { connect } from 'react-redux'
-import Link from 'next/link'
-import { Layout, Row, Col } from 'antd'
+import { connect } from 'react-redux';
+import Router from 'next/router';
+import Link from 'next/link';
+import { Layout, Avatar, Row, Col } from 'antd';
 
-const { Header } = Layout
+const { Header } = Layout;
 
 const HeaderWithRedux = ({ loginUser }) => (
   <Header>
@@ -11,11 +12,27 @@ const HeaderWithRedux = ({ loginUser }) => (
         <Link prefetch href='/'><a><h1>TableSpoon</h1></a></Link>
       </Col>
       <Col>
-        <Link href='/#about'><a className='rm'>소개</a></Link>
-        <Link prefetch href='/become-a-host'><a className='rm'>호스팅하기</a></Link>
+        <Link prefetch href='/#about'><a className='rm'>소개</a></Link>
+        <a className='rm'
+          onMouseEnter={() => {
+            Router.prefetch('/become-host');
+            console.log('prefetching /become-host!');
+          }}
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            Router.push('/become-host');
+          }}
+        >호스팅하기</a>
         {loginUser
-          ? <Link href='/logout'><a>{loginUser.email}</a></Link>
+          ? <Link prefetch href='/my/profile'><a className="rm">{loginUser.email}</a></Link>
           : <Link prefetch href='/sign'><a>회원가입/로그인</a></Link>
+        }
+        {
+          loginUser
+            ? <Avatar
+              src={loginUser.profileImage} />
+            : false
         }
       </Col>
 
@@ -37,10 +54,10 @@ const HeaderWithRedux = ({ loginUser }) => (
     `}</style>
     </Row>
   </Header>
-)
+);
 
 export default connect((state) => {
   return {
     loginUser: state.loginUser
-  }
-})(HeaderWithRedux)
+  };
+})(HeaderWithRedux);
