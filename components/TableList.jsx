0bcbb9ceeeaac9bfa9623a06b4906ccc2ac70548
불꽃남자 <miconblog@gmail.com>
2018-03-23 00:00:40 +0800

    배트맨 API 변경에 따른 수정

--- components/TableList.jsx
+++ components/TableList.jsx
@@ -1,20 +1,22 @@
 import Link from 'next/link'
 import { Card, Row, Col } from 'antd'
 
-const EventCard = ({ show }) => (
-  <Col key={show.id} span={8}>
-    <Card
-      style={{ width: 280, margin: '0 auto' }}
-      cover={<img src={show.image.medium} />}
-    >
-      <Link as={`/tables/${show.id}`} href={`/post?id=${show.id}`}>
-        <a>{show.name}</a>
-      </Link>
-    </Card>
-  </Col>
-)
+const EventCard = ({ show }) => {
+  return (
+    <Col key={show.id} span={8}>
+      <Card
+        style={{ width: 280, margin: '0 auto' }}
+        cover={show.image ? <img src={show.image.medium} /> : false}
+      >
+        <Link as={`/tables/${show.id}`} href={`/post?id=${show.id}`}>
+          <a>{show.name}</a>
+        </Link>
+      </Card>
+    </Col>
+  )
+};
 
-export default ({rows}) => (
+export default ({ rows }) => (
   <div id='table-list' className='intro'>
     <h2>예약가능한 테이블</h2>
     <div>
--- pages/post.jsx
+++ pages/post.jsx
@@ -4,43 +4,11 @@ import { HomeLayout } from '../layouts';
 import fetch from 'isomorphic-unfetch';
 import Markdown from 'react-markdown';
 
-const Post = (props) => (
+const Post = ({ show }) => (
   <HomeLayout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?tables>/g, '')}</p>
-    <img src={props.show.image.medium} />
-
-    <div className='markdown'>
-      <Markdown source={`
-This is our blog post.
-Yes. We can have a [link](/link).
-And we can have a title as well.
-
-### This is a title
-
-And here's the content.
-     `} />
-    </div>
-    <style jsx global>{`
-     .markdown {
-       font-family: 'Arial';
-     }
-
-     .markdown a {
-       text-decoration: none;
-       color: blue;
-     }
-
-     .markdown a:hover {
-       opacity: 0.6;
-     }
-
-     .markdown h3 {
-       margin: 0;
-       padding: 0;
-       text-transform: uppercase;
-     }
-  `}</style>
+    <h1>{show.name}</h1>
+    <div dangerouslySetInnerHTML={{ __html: show.summary.replace(/<[/]?tables>/g, '') }} />
+    {show.image ? <img src={show.image.medium} /> : false}
   </HomeLayout>
 );
 
