6ec437e2661a53e49e22b3346533cd8a3971c9f5
불꽃남자 <miconblog@gmail.com>
2018-03-24 23:20:42 +0800

    withRedux 리팩토링
    주요 변경
    1. __NEXT_REDUX_STORE__ 를 심볼로 선언해서 전역변수 방어
    2. props와 context 인자 이름 구분
    3. 주석 추가
    4. 변수 선언문은 코드 가독성을 위해 한줄로 선언

--- redux/withRedux.js
+++ redux/withRedux.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect, Provider } from 'react-redux';
 
-const __NEXT_REDUX_STORE__ = '__NEXT_REDUX_STORE__';
+const __NEXT_REDUX_STORE__ = Symbol('__NEXT_REDUX_STORE__');
 
 // https://github.com/iliakan/detect-node
 const checkServer = () => Object.prototype.toString.call(global.process) === '[object process]';
@@ -23,19 +23,29 @@ export default (...args) => (Component) => {
   // First argument is initStore, the rest are redux connect arguments and get passed
   const [initStore, ...connectArgs] = args;
 
-  //console.log('1. withRedux 초기화');
-
-
-  // 마지막에 동작하는 컴포넌트,..
-  const ComponentWithRedux = (props = {}) => {
-    const { store, initialProps = {}, initialState } = props;
-    //console.log('4. withRedux 랩핑 컴포넌트가 마지막으로 반응한다.')
+  /**
+   * 반환할 HoC 컴포넌트 
+   * 
+   * @param {*} props - getInitialProps 에서 반환하는 정보
+   * store - 리덕스 스토어 인스턴스
+   * initialProps - 컴포넌트 초기값
+   * initialState - 스토어 초기값
+   */
+  const ComponentWithRedux = (props) => {
+    const { store, initialProps, initialState } = props;
 
     // Connect page to redux with connect arguments
     const ConnectedComponent = connect.apply(null, connectArgs)(Component);
 
-    // Wrap with redux Provider with store
-    // Create connected page with initialProps
+    /** 
+     * Wrap with redux Provider with store
+     * Create connected page with initialProps
+     * 
+     * 아래와 같이 Provider로 원본 컴포넌트를 감싸서 반환하면 스토어와 연결된다. 
+     *   <Provider store={store}>
+     *     <ConnectedComponent {...initialProps} />
+     *   </Provider>
+     */
     return React.createElement(
       Provider, {
         store: store && store.dispatch ? store : getOrCreateStore(initStore, initialState)
@@ -44,35 +54,44 @@ export default (...args) => (Component) => {
     );
   };
 
-  // Next.js --> getInitialProps (S)
-  ComponentWithRedux.getInitialProps = async (props = {}) => {
+  /**
+   * Next.js 가 호출한 static getInitialProps 함수 재정의
+   * 전역으로 사용할 인증(loginUser) 객체를 리덕스 스토어(store)에 저장한다. 
+   * 
+   * @param {*} context - Next.js 가 주입하는 정보
+   * pathname - path section of URL
+   * query - query string section of URL parsed as an object
+   * asPath - String of the actual path (including the query) shows in the browser
+   * req - HTTP request object (server only)
+   * res - HTTP response object (server only)
+   * jsonPageRes - Fetch Response object (client only)
+   * err - Error object if any error is encountered during the rendering
+   */
+  ComponentWithRedux.getInitialProps = async (context) => {
     const isServer = checkServer();
-    //console.log('2. Next.js 에서 랩핑한 getInitialProps 호출 하지만 서버니?:', isServer);
 
     // 스토어 객체에 인증정보를 같이 넣는다.
-    const { req } = props;
+    const { req } = context;
     let loginUser = null;
 
     if (isServer && req.user) {
       // for server
       loginUser = req.user.toJSON();
-      loginUser.profileImage = "https://scontent-hkg3-2.xx.fbcdn.net/v/t1.0-1/p320x320/1510494_10151938283373302_404117394_n.jpg?oh=fef97d5f281b11e85993992d276ee11f&oe=5B45B569";
     } else {
       // for client..
       loginUser = store && store.getState().loginUser;
     }
 
+    // 프로필 이미지가 없으면 샘플을 넣어준다.
+    if (loginUser && !loginUser.profileImage) {
+      loginUser.profileImage = "/assets/images/default-user-image.png";
+    }
+
+    // 인증 객체를 스토어에 넣어서 초기화 한다.
     const store = getOrCreateStore(initStore, { loginUser });
 
-    // Run page getInitialProps with store and isServer + loginUser
-    // 서버에서 동작: HOC 원본 컴포넌트에 스토어와 인증객체 같이 내려줌. 
-    const initialProps = Component.getInitialProps
-      ? await Component.getInitialProps({
-        ...props,
-        isServer,
-        store
-      })
-      : {};
+    // 원본 getInitialProps 를 직접 호출해서 디폴트로 props로 주입할 initialProps 값을 얻어온다.
+    const initialProps = Component.getInitialProps ? await Component.getInitialProps({ ...context, isServer, store }) : {};
 
     // HOC 컴포넌트는 원본이 먼저 동작한 후에 반환된다. for Client
     return {
