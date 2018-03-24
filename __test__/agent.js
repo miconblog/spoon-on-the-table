d0704138e490c8a22d6ef1e6ec31fb68375dfa4e
불꽃남자 <miconblog@gmail.com>
2018-03-24 23:22:36 +0800

    테스트 작성을 위해 랜더링 코드와 API 코드를 분리하고 supertest를 랩핑한 테스트 에이전트를 추가함.

---
+++ __test__/agent.js
@@ -0,0 +1,28 @@
+import server from '../server';
+import request from 'supertest';
+
+server.listen(3000, (err) => {
+  if (err) throw err;
+  console.info('> Ready on http://localhost:3000');
+});
+
+export default ({ cookie, method, url, data }) => {
+  return new Promise((resolve) => {
+
+    const query = request(server)[method.toLowerCase()](url)
+      .set('Accept', 'application/json');
+
+    if (cookie) {
+      query.set('cookie', cookie);
+    }
+
+    query
+      .send(data)
+      .expect('Content-Type', /json/)
+      .then(({ header, status, body }) => resolve({ header, status, body }))
+      .catch(ex => console.log(ex))
+
+  }).catch((ex) => {
+    throw new Error(ex);
+  });
+}
