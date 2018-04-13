a784af1c6fe1ff49f66307cc3235bca644bea33a
불꽃남자 <miconblog@gmail.com>
2018-04-13 23:53:47 +0900

    임시 테이블 조회 및 저장 with test

--- apps/restapi/routes/api/table/index.js
+++ apps/restapi/routes/api/table/index.js
@@ -5,15 +5,47 @@ const bodyParser = require('body-parser');
 const cloudReq = require('../../../../lib/request-parse-cloud');
 const authentication = require('../../../../lib/authentication');
 
-function getUserTableCache(req, res) {
+const UserCache = Parse.Object.extend('UserCache');
+
+async function getUserTableCache(req, res) {
   const { user, params: { id } } = req;
   const sessionToken = user.getSessionToken();
 
-  res.json({ 'result': 'OK' })
+  // UserCache를 member로 조회해서 일단 있는지 확인 
+
+  const query = new Parse.Query(UserCache);
+
+  query.equalTo('member', user);
+  let cache = await query.first();
+
+  // 캐시가 없으면 항상 생성한다.
+  if (!cache) {
+    cache = new UserCache();
+    cache.set('member', user);
+    await cache.save();
+  }
+
+  res.json({ data: cache.toJSON() })
+}
+
+async function saveUserTableCache(req, res) {
+
+  const { user, body: { table } } = req;
+
+  // UserCache를 member로 조회해서 일단 있는지 확인 
+  const query = new Parse.Query(UserCache);
+  query.equalTo('member', user);
+
+  const cache = await query.first();
+  cache.set('table', table);
+  await cache.save();
+
+  res.json({ data: cache.toJSON() })
 }
 
 // 개인정보수정은 로그인한 본인만 할 수 있음.
 router.get('/temporary', authentication, getUserTableCache);
+router.put('/temporary', authentication, bodyParser.json(), saveUserTableCache);
 
 module.exports = {
   router
