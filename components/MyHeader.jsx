ef2742b8badfe7b5e72427083d0e69e573c91c75
불꽃남자 <miconblog@gmail.com>
2018-05-15 17:53:36 +0900

    헤더 영역도 모바일에서 반응형으로 동작해야한다.

--- components/MyHeader.jsx
+++ components/MyHeader.jsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'next/router'
 import Router from 'next/router';
 import Link from 'next/link';
-import { Layout, Row, Col } from 'antd';
+import { Layout, Row, Col, Icon } from 'antd';
 import TableSpoonLogo from './TableSpoonLogo';
 import PrefetchLink from './PrefetchLink';
 
@@ -14,28 +14,30 @@ const HeadStyle = {
 };
 
 const Menus = [
-  { href: '/become-a-host', title: '테이블 추가' },
-  { href: '/my-tables', as: '/my/tables', title: '호스트' },
-  { href: '/my-profile', as: '/my/profile', title: '내정보' },
+  { href: '/become-a-host', title: '테이블 추가', icon:'plus' },
+  { href: '/my-tables', as: '/my/tables', title: '호스트', icon:'solution' },
+  { href: '/my-profile', as: '/my/profile', title: '내정보', icon:'user' },
   // { href: '/my-inbox', as: '/my/inbox', title: '메시지' },
   // { href: '/my-guests', as: '/my/guests', title: '예약관리' },
 ];
 
 const MyHeaderWithRedux = ({ router }) => {
   return (
-    <Header className="bright" style={HeadStyle}>
+    <Header className="bright header" style={HeadStyle}>
       <Row type='flex' justify='space-between'>
         <Col>
           <TableSpoonLogo />
         </Col>
         <Col>
-          {Menus.map(({ href, as, title }) => {
+          {Menus.map(({ href, as, title, icon }) => {
             const style = {
               color: router.pathname === href ? 'red' : 'black'
             }
 
             return (
-              <PrefetchLink key={title} style={style} href={href} as={as}>{title}</PrefetchLink>
+              <PrefetchLink key={title} style={style} href={href} as={as}>
+                <Icon type={icon}/> {title}
+              </PrefetchLink>
             )
           })}
         </Col>
