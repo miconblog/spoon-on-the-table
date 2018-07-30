a9d08311467c471c8b06f70e10a533c60bc24397
불꽃남자 <miconblog@gmail.com>
2018-07-31 00:54:35 +0900

    최신 모듈로 업데이트 및 styled-jsx 의존성 제거
    styled- 형태의 스타일링이 기존의 preprocessor 를 이용한 방법보다 불편하다. (디버깅 힘들었다 ㅜㅜ)

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import stylesheet from 'antd/dist/antd.min.css';
 import Document, { Head, Main, NextScript } from 'next/document';
-import flush from 'styled-jsx/server';
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const { html, head, errorHtml, chunks } = renderPage();
-    const styles = flush();
-    return { html, head, errorHtml, chunks, styles };
+    return { html, head, errorHtml, chunks };
   }
 
   render() {
