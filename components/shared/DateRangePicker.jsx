a9e01e33b6c95a285181c2e5d0f72388a985e16b
불꽃남자 <miconblog@gmail.com>
2018-10-27 19:29:54 +0900

    검색에서 쓰일 날짜 선택 컴포넌트 공통 모듈로 변경

--- components/shared/DateRangePicker.jsx
+++ components/shared/DateRangePicker.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unused-prop-types */
 import React from 'react';
+import { Button } from 'antd';
 import omit from 'lodash/omit';
 import { DayPickerRangeController, isInclusivelyAfterDay } from 'react-dates';
 import { START_DATE, END_DATE } from 'react-dates/constants';
@@ -14,18 +15,19 @@ class DateRangePicker extends React.Component {
 
     this.state = {
       focusedInput: props.autoFocusEndDate ? END_DATE : START_DATE,
-      startDate: props.initialStartDate,
-      endDate: props.initialEndDate,
+      // startDate: props.initialStartDate,
+      // endDate: props.initialEndDate,
     };
 
     this.onDatesChange = this.onDatesChange.bind(this);
     this.onFocusChange = this.onFocusChange.bind(this);
+    this.onClear = this.onClear.bind(this);
   }
 
   onDatesChange({ startDate, endDate }) {
     const { onDatesChange } = this.props;
 
-    this.setState({ startDate, endDate });
+    // this.setState({ startDate, endDate });
     onDatesChange(startDate, endDate);
   }
 
@@ -36,27 +38,44 @@ class DateRangePicker extends React.Component {
     });
   }
 
+  onClear() {
+    const { onClear } = this.props;
+    this.setState({
+      focusedInput: START_DATE,
+    });
+    onClear();
+  }
+
   render() {
-    const { focusedInput, startDate, endDate } = this.state;
+    const { focusedInput } = this.state;
+    const { onApplay, startDate, endDate } = this.props;
 
     const props = omit(this.props, [
       'autoFocus',
       'autoFocusEndDate',
       'initialStartDate',
       'initialEndDate',
+      'onApplay',
+      'onClear',
     ]);
 
     return (
-      <DayPickerRangeController
-        {...props}
-        isOutsideRange={day => !isInclusivelyAfterDay(day, moment())}
-        hideKeyboardShortcutsPanel
-        onDatesChange={this.onDatesChange}
-        onFocusChange={this.onFocusChange}
-        focusedInput={focusedInput}
-        startDate={startDate}
-        endDate={endDate}
-      />
+      <div className="DateRangePicker">
+        <DayPickerRangeController
+          {...props}
+          isOutsideRange={(day) => !isInclusivelyAfterDay(day, moment())}
+          hideKeyboardShortcutsPanel
+          onDatesChange={this.onDatesChange}
+          onFocusChange={this.onFocusChange}
+          focusedInput={focusedInput}
+          startDate={startDate}
+          endDate={endDate}
+        />
+        <div className="btn-group">
+          <Button onClick={this.onClear}>삭제</Button>
+          <Button onClick={onApplay}>적용</Button>
+        </div>
+      </div>
     );
   }
 }
